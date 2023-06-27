2f9f50c78d0b90a34689fef5d3f631b496604284
zhaozhentao <qq344696734@gmail.com>
2023-06-27 16:15:54 +0800

    remove useless code

--- src/main/java/io/tg/minix/ResultAdapter.java
+++ src/main/java/io/tg/minix/ResultAdapter.java
@@ -2,7 +2,6 @@ package io.tg.minix;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
